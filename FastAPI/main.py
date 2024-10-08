f489277b95fce84d704c319685d6c10ace7496a7
iDivey <150763080+iDivey@users.noreply.github.com>
2024-10-08 11:33:33 +0700

    UPD. FastAPI

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,17 +1,16 @@
+from datetime import datetime
 from fastapi import FastAPI, Request, Depends, Form
-from sqlalchemy import insert
+from sqlalchemy import insert, desc, select
 from sqlalchemy.orm import Session
-from starlette.responses import RedirectResponse
-
 from models import *
 from database import engine, session_local
-from schemes import *
 from fastapi.templating import Jinja2Templates
 from typing import Annotated
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+
 
 def get_db():
     db = session_local()
@@ -28,39 +27,80 @@ templates = Jinja2Templates(directory='templates')
 async def main_page(request: Request):
     return templates.TemplateResponse('main.html', {'request': request})
 
+
 @app.get('/book')
 async def book_page(request: Request):
     return templates.TemplateResponse('book.html', {'request': request})
 
+
 @app.post('/book')
 async def book_result(request: Request, db: Annotated[Session, Depends(get_db)],
-                author: str = Form(), name_father_Author: str = Form(), title: str = Form(), publisher: str = Form(), city: str = Form(), year: int = Form(), pages: int = Form()):
-        db.execute(insert(Book).values(
-            author=author,
-            name_father_Author=name_father_Author,
-            title=title,
-            publisher=publisher,
-            city=city,
-            year=year,
-            pages=pages
-        ))
-        db.commit()
-        return RedirectResponse('/book_final')
-
-
-@app.get('/book_final')
-def book_final_page(request: Request, db: Annotated[Session, Depends(get_db)]):
-    book = Book.query.order_by(Book.id).desc().first()
-    return templates.TemplateResponse('book_final.html', {'request': request,'book': book})
+                      author: str = Form(), name_father_Author: str = Form(), title: str = Form(),
+                      publisher: str = Form(), city: str = Form(), year: int = Form(), pages: int = Form()):
+    db.execute(insert(Book).values(
+        author=author,
+        name_father_Author=name_father_Author,
+        title=title,
+        publisher=publisher,
+        city=city,
+        year=year,
+        pages=pages
+    ))
+    db.commit()
+    book = db.query(Book).order_by(desc(Book.id)).first()
+    return templates.TemplateResponse('book_final.html', {'request': request, 'book': book})
 
 
 @app.get('/conf')
 async def conf_page(request: Request):
     return templates.TemplateResponse('conf.html', {'request': request})
 
+
+@app.post('/conf')
+async def conf_result(request: Request, db: Annotated[Session, Depends(get_db)],
+                      author: str = Form(), name_father_Author: str = Form(), title: str = Form(),
+                      publisher: str = Form(), place: str = Form(), date: datetime = Form(), city: str = Form(),
+                      year: int = Form(), page_start: int = Form(), page_end: int = Form()):
+    db.execute(insert(Conf).values(
+        author=author,
+        name_father_Author=name_father_Author,
+        title=title,
+        publisher=publisher,
+        city=city,
+        place=place,
+        date=date,
+        year=year,
+        page_start=page_start,
+        page_end=page_end
+    ))
+    db.commit()
+    conf = db.query(Conf).order_by(desc(Conf.id)).first()
+    return templates.TemplateResponse('conf_final.html', {'request': request, 'conf': conf})
+
+
 @app.get('/journal')
 async def journal_page(request: Request):
     return templates.TemplateResponse('journal.html', {'request': request})
+
+
+@app.post('/journal')
+async def journal_result(request: Request, db: Annotated[Session, Depends(get_db)],
+                      author: str = Form(), name_father_Author: str = Form(), title: str = Form(),
+                      publisher: str = Form(), number_tom: int = Form(), year: int = Form(), page_start: int = Form(), page_end: int = Form()):
+    db.execute(insert(Jour).values(
+        author=author,
+        name_father_Author=name_father_Author,
+        title=title,
+        publisher=publisher,
+        number_tom=number_tom,
+        year=year,
+        page_start=page_start,
+        page_end=page_end
+    ))
+    db.commit()
+    journal = db.query(Jour).order_by(desc(Jour.id)).first()
+    return templates.TemplateResponse('journal_final.html', {'request': request, 'journal': journal})
+
 
 @app.get('/descript')
 async def journal_page(request: Request):
--- FastAPI/models.py
+++ FastAPI/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
 from database import Base
 
 
@@ -13,3 +13,33 @@ class Book(Base):
     city = Column(String(20), nullable=False)
     year = Column(Integer, nullable=False)
     pages = Column(Integer, nullable=False)
+
+
+class Conf(Base):
+    __tablename__ = 'conference'
+    __table_args__ = {'extend_existing': True}
+    id = Column(Integer, primary_key=True, index=True)
+    author = Column(String(30), nullable=False)
+    name_father_Author = Column(String(5), nullable=False)
+    title = Column(String, nullable=False)
+    publisher = Column(String(20), nullable=False)
+    place = Column(String(20), nullable=False)
+    date = Column(DateTime, nullable=False)
+    city = Column(String(20), nullable=False)
+    year = Column(Integer, nullable=False)
+    page_start = Column(Integer, nullable=False)
+    page_end = Column(Integer, nullable=False)
+
+
+class Jour(Base):
+    __tablename__ = 'journal'
+    __table_args__ = {'extend_existing': True}
+    id = Column(Integer, primary_key=True, index=True)
+    author = Column(String(30), nullable=False)
+    name_father_Author = Column(String(5), nullable=False)
+    title = Column(String, nullable=False)
+    publisher = Column(String(20), nullable=False)
+    number_tom = Column(Integer, nullable=False)
+    year = Column(Integer, nullable=False)
+    page_start = Column(Integer, nullable=False)
+    page_end = Column(Integer, nullable=False)
